1977f75079b7f418e011c482872d450cbcf6daa8
Nair Suraj Gangadharan <nsgarafaite@gmail.com>
2021-06-30 15:53:12 +0530

    Adding Shop Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom"
 import HomePage from './pages/homepage/home-page.component'
+import ShopPage from '../src/pages/shoppage/shoppage.component'
+
+
+const HatsPage = () => (
+   <div>
+      <h1>HatsPage</h1>
+   </div>
+)
+
 
 function App() {
   return (
     <div className="App">
-    <HomePage/>
+      <Switch>
+        <Route exact path ='/' component={HomePage}/>
+        <Route exact path ='/shop/hats' component={HatsPage}/>
+        <Route exact path ='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import {withRouter} from 'react-router-dom';
 
 
-export const MenuItem = ({title,imageUrl,size}) => (
-<div className = {`${size} menu-item`}>
+ const MenuItem = ({title,imageUrl,size,history, linkUrl, match}) => (
+<div className = {`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
     <div 
         className = 'background-image'
         style = {{
@@ -19,3 +20,5 @@ export const MenuItem = ({title,imageUrl,size}) => (
 
 </div>
 )
+
+export default withRouter(MenuItem)
